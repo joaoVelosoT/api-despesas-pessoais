7077d8d5ae08ed382f57ccf0cc1632362807e421
João Veloso <141861100+joaoVelosoT@users.noreply.github.com>
2024-10-20 13:40:31 -0300

    termino do script de historico de transferencia

--- view/tela-inicial/telaInicial.js
+++ view/tela-inicial/telaInicial.js
@@ -56,21 +56,21 @@ const totalEntradas = async () => {
 };
 
 const totalSaidas = async () => {
-    try {
-      const totalSaidas = document.getElementById("total-saidas");
-      const token = sessionStorage.getItem("token");
-  
-      const response = await fetch("http://localhost:3000/transicao/saidas", {
-        method: "GET",
-        headers: { Authorization: `${token}` },
-      });
-  
-      const json = await response.json();
-      console.log(json)
-      totalSaidas.innerText = `R$${json.totalSaidas}`;
-    } catch (error) {
-      console.error(error);
-    }
+  try {
+    const totalSaidas = document.getElementById("total-saidas");
+    const token = sessionStorage.getItem("token");
+
+    const response = await fetch("http://localhost:3000/transicao/saidas", {
+      method: "GET",
+      headers: { Authorization: `${token}` },
+    });
+
+    const json = await response.json();
+    console.log(json);
+    totalSaidas.innerText = `R$${json.totalSaidas}`;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 const transacao = async () => {
@@ -80,118 +80,179 @@ const transacao = async () => {
   const form = document.getElementById("transaction-form");
   const msgError = document.getElementsByClassName("error-message")[0];
 
-
-  form.addEventListener('submit', async(e) => {
+  form.addEventListener("submit", async (e) => {
     e.preventDefault();
 
-    if(!valor.value || !descricao.value || !tipo.value){
-      msgError.textContent = "Envie todos os dados !"
-      msgError.style.display = "block"
-      return 
+    if (!valor.value || !descricao.value || !tipo.value) {
+      msgError.textContent = "Envie todos os dados !";
+      msgError.style.display = "block";
+      return;
     }
 
-    if(valor.value < 0){
-      msgError.textContent = "O valor não pode ser negativo"
-      msgError.style.display = "block"
-      return 
+    if (valor.value < 0) {
+      msgError.textContent = "O valor não pode ser negativo";
+      msgError.style.display = "block";
+      return;
     }
 
     msgError.textContent = "";
     msgError.style.display = "none";
-    
+
     const data = JSON.stringify({
-      valor : valor.value,
-      descricao : descricao.value,
-      tipo : tipo.value
-    })
+      valor: valor.value,
+      descricao: descricao.value,
+      tipo: tipo.value,
+    });
 
     try {
       const token = sessionStorage.getItem("token");
-      const response = await fetch('http://localhost:3000/transicao/', {
-        method : 'POST',
+      const response = await fetch("http://localhost:3000/transicao/", {
+        method: "POST",
         headers: {
-           Authorization: `${token}`,
-           "Content-Type": "application/json; charset=UTF-8"
-          },
-        body : data
-      })
+          Authorization: `${token}`,
+          "Content-Type": "application/json; charset=UTF-8",
+        },
+        body: data,
+      });
 
       const json = await response.json();
 
       saldoTotal();
       totalEntradas();
       totalSaidas();
-      alert("Transação feita com sucesso !")
+      todasTransferencias();
+      alert("Transação feita com sucesso !");
       valor.value = "";
       descricao.value = "";
-      
     } catch (error) {
       console.error(error);
     }
-
-
-
-
-  })
-}
+  });
+};
 
 const todasTransferencias = async () => {
   try {
 
+    // Selecionando o elemento container no HTML
     const historico = document.getElementById("transaction-history");
+
+    historico.innerHTML = ""
+    // Pegando o token no sessionStorage
     const token = sessionStorage.getItem("token");
+
+    // Pegando o input do tipo no HTML
     const tipo = document.getElementById("filter-type");
 
-    addEventListener('change', async(e) => {
+    // Fazendo o fetch de todas as transferencias
+    const response = await fetch(
+      "http://localhost:3000/transicao/todastransferencias",
+      {
+        method: "GET",
+        headers: { Authorization: `${token}` },
+      }
+    );
 
-      const tipo = tipo.value
-      console.log(tipo.value)
-    
-      const response = await fetch(url, {
-        method : 'GET',
-        headers: {
-          Authorization: `${token}`,
-          // "Content-Type": "application/json; charset=UTF-8"
-         },
-      })
-      const json = await response.json();
-      const transferencias = json.transacoes;
-      transferencias.forEach(element => {
-        console.log(element);
-        historico.innerHTML += `<li class="transaction ${element.tipo}">
-                      <span>${element.descricao}</span>
-                      <span class="amount">R$ 2500,00</span>
-                  </li>`
-      });
-    
-    })
-
-    const response = await fetch('http://localhost:3000/transicao/todastransferencias', {
-      method : 'GET',
-      headers: {
-        Authorization: `${token}`,
-        // "Content-Type": "application/json; charset=UTF-8"
-       },
-    })
+    // Pegando o retorno do fetch, e convertendo para um json
     const json = await response.json();
-    const transferencias = json.transacoes;
-    transferencias.forEach(element => {
-      console.log(element);
+
+    // Selecionando apenas as transacoes do fetch
+    const transacoes = json.transacoes;
+
+    // Fazendo o forEach para jogar as transacoes no HTML
+    transacoes.forEach((element) => {
       historico.innerHTML += `<li class="transaction ${element.tipo}">
-                    <span>${element.descricao}</span>
-                    <span class="amount">R$ 2500,00</span>
-                </li>`
+                      <span>${element.descricao}</span>
+                      <span class="amount">R$ ${element.valor}</span>
+                  </li>`;
     });
 
+    // Adicionando o evento no input de tipo
+    tipo.addEventListener("change", async (e) => {
+      switch (tipo.value) {
+        case "entrada":
+          // Limpando o HTML
+          historico.innerHTML = "";
 
-    
+          const response = await fetch(
+            "http://localhost:3000/transicao/todasentradas",
+            {
+              method: "GET",
+              headers: { Authorization: `${token}` },
+            }
+          );
+
+          const json = await response.json();
+          // console.log(json)
+          const transacoes = json.entradas;
+
+          transacoes.forEach((element) => {
+            historico.innerHTML += `<li class="transaction ${element.tipo}">
+                            <span>${element.descricao}</span>
+                            <span class="amount">R$ ${element.valor}</span>
+                        </li>`;
+          });
+
+          break;
+        case "saida":
+
+          // Limpando o HTML
+          historico.innerHTML = "";
+
+          // console.log("ta na entrada")
+
+          const responseSaida = await fetch(
+            "http://localhost:3000/transicao/todassaidas",
+            {
+              method: "GET",
+              headers: { Authorization: `${token}` },
+            }
+          );
+
+          const jsonSaida = await responseSaida.json();
+          // console.log(json)
+          const transacoesSaida = jsonSaida.saidas;
+          console.log(transacoesSaida);
+          transacoesSaida.forEach((element) => {
+            historico.innerHTML += `<li class="transaction ${element.tipo}">
+                            <span>${element.descricao}</span>
+                            <span class="amount">R$ ${element.valor}</span>
+                        </li>`;
+          });
+          break;
+        case "todos":
+          // Limpando o HTML
+          historico.innerHTML = "";
+
+          // Fazendo o fetch de todas as transferencias
+          const responseTodas = await fetch(
+            "http://localhost:3000/transicao/todastransferencias",
+            {
+              method: "GET",
+              headers: { Authorization: `${token}` },
+            }
+          );
+
+          // Pegando o retorno do fetch, e convertendo para um json
+          const jsonTodas = await responseTodas.json();
+
+          // Selecionando apenas as transacoes do fetch
+          const todasTransferencias = jsonTodas.transacoes;
+
+          // Fazendo o forEach para jogar as transacoes no HTML
+          todasTransferencias.forEach((element) => {
+            historico.innerHTML += `<li class="transaction ${element.tipo}">
+                      <span>${element.descricao}</span>
+                      <span class="amount">R$ ${element.valor}</span>
+                  </li>`;
+          });
+
+          break;
+      }
+    });
   } catch (error) {
     console.error(error);
   }
-}
-
-
-
+};
 
 transacao();
 logout();
